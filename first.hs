dd2a6ea0b7ba20787b3c8a47c9d43806c81509e7
abhishek.healthify <abhishek.bharadwaj@healthify.co>
2017-10-03 11:59:11 +0530

    Init commit

--- first.hs
+++ first.hs
@@ -25,3 +25,15 @@ pickNumber numb
     | numb <= 10.0  = "Eh! Playing safe"
     | numb <= 100.0 = "You got a big number.."
     | otherwise     = "You moron"
+
+
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
+    | otherwise     = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          skinny = 18.5  
+          normal = 25.0  
+          fat = 30.0
